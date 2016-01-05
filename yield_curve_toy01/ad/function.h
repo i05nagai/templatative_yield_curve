2e8ae3db09701b8d96db4443cc858f7f4b56d120
i05nagai <i05nagai@gmail.com>
2016-01-06 00:33:24 +0900

    Modified traits.

--- yield_curve_toy01/ad/function.h
+++ yield_curve_toy01/ad/function.h
@@ -3,14 +3,42 @@
 
 #include "dual.h"
 #include "functor.h"
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits.hpp>
 
 namespace ad {
     /*
      * exponetial function.
      */
+    template <typename E, typename T>
+    struct exp_traits {
+    private:
+        typedef E value_type;
+    public:
+        typedef value_type result_type;
+        static result_type apply(const E& e)
+        {
+            return std::exp(e);
+        }
+    };
 
     template <typename E>
-    struct exp_traits<vector_expression<E> > {
+    struct exp_traits<dual<E> > {
+    private:
+    public:
+        typedef E value_type;
+        typedef dual<E> result_type;
+    public:
+        static result_type apply(const dual<E>& e)
+        {
+            return result_type(std::exp(e.v()), e.d() * std::exp(e.v()));
+        }
+    };
+
+    template <typename E>
+    struct exp_traits<E,
+        typename boost::enable_if< 
+            boost::is_base_of<vector_expression<E>, E> >::type> {
     private:
         typedef typename E::value_type value_type;
         typedef scalar_exp<value_type> functor_type;
@@ -23,30 +51,44 @@ namespace ad {
         }
     };
 
-    double exp(const double e)
-    {
-        return std::exp(e);
-    }
-
-    template <typename E> dual<E> exp(const dual<E>& e)
-    {
-        return dual<E>(std::exp(e.v()), e.d() * std::exp(e.v()));
-    }
-
     template <typename E>
-    typename exp_traits<vector_expression<E> >::result_type 
-    exp(const vector_expression<E>& e)
+    typename exp_traits<E>::result_type exp(const E& e)
     {
-        typedef exp_traits<vector_expression<E> > Tr;
+        typedef exp_traits<E> Tr;
         return Tr::apply(e);
     }
 
     /*
      * logarithmic function.
      */
-    //E is vector_expression
+    template <typename E, typename T>
+    struct log_traits {
+    private:
+        typedef E value_type;
+    public:
+        typedef value_type result_type;
+        static result_type apply(const E& e)
+        {
+            return std::log(e);
+        }
+    };
+
     template <typename E>
-    struct log_traits<vector_expression<E> > {
+    struct log_traits<dual<E> > {
+    private:
+    public:
+        typedef E value_type;
+        typedef dual<E> result_type;
+        static result_type apply(const dual<E>& e)
+        {
+            return result_type(std::log(e.v()), e.d() / e.v());
+        }
+    };
+
+    template <typename E>
+    struct log_traits<E,
+        typename boost::enable_if< 
+            boost::is_base_of<vector_expression<E>, E> >::type> {
     private:
         typedef typename E::value_type value_type;
         typedef scalar_log<value_type> functor_type;
@@ -59,21 +101,10 @@ namespace ad {
         }
     };
 
-    double log(const double e)
-    {
-        return std::log(e);
-    }
-
-    template <typename E> dual<E> log(const dual<E>& e)
-    {
-        return dual<E>(std::log(e.v()), e.d() / e.v());
-    }
-
     template <typename E>
-    typename log_traits<vector_expression<E> >::result_type 
-        log(const vector_expression<E>& e)
+    typename log_traits<E>::result_type log(const E& e)
     {
-        typedef log_traits<vector_expression<E> > Tr;
+        typedef log_traits<E> Tr;
         return Tr::apply(e);
     }
 
--- yield_curve_toy01/ad/fwd.h
+++ yield_curve_toy01/ad/fwd.h
@@ -1,6 +1,9 @@
 #ifndef AD_FWD_H_INCLUDED
 #define AD_FWD_H_INCLUDED
 
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits.hpp>
+
 namespace ad {
 
     template <typename E> class vector_expression;
@@ -8,20 +11,14 @@ namespace ad {
     template <typename T> class dual;
 
     //exp
-    template <typename E> struct exp_traits;
-    double exp(const double e);
-    template <typename E> dual<E> exp(const dual<E>& e);
-    template <typename E>
-        typename exp_traits<vector_expression<E> >::result_type 
-            exp(const vector_expression<E>& e);
+    template <typename E, typename T = void> struct exp_traits;
+    template <typename E> 
+        typename exp_traits<E>::result_type exp(const E& e);
 
     //log
-    template <typename E> struct log_traits;
-    double log(const double e);
-    template <typename E> dual<E> log(const dual<E>& e);
-    template <typename E>
-        typename log_traits<vector_expression<E> >::result_type 
-            log(const vector_expression<E>& e);
+    template <typename E, typename T = void> struct log_traits;
+    template <typename E> 
+        typename log_traits<E>::result_type log(const E& e);
 } // namespace ad {
 
 #endif // #ifndef AD_FWD_H_INCLUDED
