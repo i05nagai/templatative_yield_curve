0ce7a328d1d451c007a85a9bbfbd7abba7f118a0
i05nagai <i05nagai@gmail.com>
2016-01-16 18:23:13 +0900

    Added public/private access specifies type_traits.

--- yield_curve_toy01/ad/type_traits.h
+++ yield_curve_toy01/ad/type_traits.h
@@ -1,15 +1,19 @@
 #ifndef AD_TYPE_TRAITS_H_INCLUDED
 #define AD_TYPE_TRAITS_H_INCLUDED
 
+#include "fwd.h"
+
 namespace ad {
+    // default traits
     template <typename T>
     struct type_traits {
+    private:
         typedef type_traits<T> self_type;
+    public:
         typedef T value_type;
         typedef const T& const_reference;
         typedef T& reference;
     };
-
 
     //TODO: need to implement.
     template <typename T1, typename T2>
