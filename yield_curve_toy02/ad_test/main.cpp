208d2b8ea33c7bac37633cecbfdc6aca6df1cd8a
i05nagai <i05nagai@gmail.com>
2016-02-26 07:35:12 +0900

    Rename jacobian to jacobian_matrix and adds some test file.

--- yield_curve_toy02/ad_test/main.cpp
+++ yield_curve_toy02/ad_test/main.cpp
@@ -4,6 +4,8 @@
 #include "ad_test/log_test.h"
 #include "ad_test/invert_test.h"
 #include "ad_test/expression_template_test.h"
+#include "ad_test/jacobian_matrix_test.h"
+#include "ad_test/type_traits_test.h"
 //#include "ad_test/automatic_differentiation_test.h"
 #include "ad_test/dual_test.h"
 
@@ -15,6 +17,8 @@ int main(int argc, char const* argv[])
     ad_test::invert_test();
     ad_test::dual_test();
     ad_test::expression_template_test();
+    ad_test::jacobian_matrix_test();
+    ad_test::type_traits_test();
 
 //    ad_test::automatic_differentiation_test();
 
---
+++ yield_curve_toy02/ad_test/type_traits_test.h
@@ -0,0 +1,30 @@
+#ifndef AD_TEST_TYPE_TRAITS_TEST_H_INCLUDED
+#define AD_TEST_TYPE_TRAITS_TEST_H_INCLUDED
+
+namespace ad_test {
+    namespace ublas = boost::numeric::ublas;
+    bool type_traits_test() {
+        std::cout << "---------type_traits start-------" << std::endl;
+
+        //case: is_vector
+        {
+        }
+
+        //case: is_vector_dual
+        {
+        }
+
+        //case: is_scalar_dual
+        {
+        }
+
+        //case: is_dual
+        {
+        }
+
+        std::cout << "---------type_traits end---------" << std::endl;
+        return true;
+    }
+} // namespace ad_test {
+
+#endif // #ifndef AD_TEST_TYPE_TRAITS_TEST_H_INCLUDED
