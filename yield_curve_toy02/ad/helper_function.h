1f6245a80cf15c852741b4dfe8e779b3763c6deb
i05nagai <nagai.m.ac@t.titech.ac.jp>
2016-03-28 07:10:00 +0900

    Adds jacobian matrix adapter class.

--- yield_curve_toy02/ad/helper_function.h
+++ yield_curve_toy02/ad/helper_function.h
@@ -1,16 +1,46 @@
+/**
+ * @file helper_function.h
+ * @brief 
+ * @author i05nagai
+ * @version 0.0.1
+ * @date 2016-03-27
+ */
+
 #ifndef DDD_AD_HELPER_FUNCTION_H_INCLUDED
 #define DDD_AD_HELPER_FUNCTION_H_INCLUDED
 
 namespace ddd { namespace ad {
     namespace ublas = boost::numeric::ublas;
+
+    /**
+     * @brief Making constant dual object.
+     *
+     * @tparam V type of constant value.
+     * @param v constant value.
+     * @param size the number of variables.
+     *
+     * @return constant dual object.
+     */
     template <typename V>
-    dual<V> make_constant(const V& v, const std::size_t size = 0)
+    dual<V> 
+    make_constant(const V& v, const std::size_t size = 0)
     {
         return dual<V>(v, ublas::vector<V>(size));
     }
 
+    /**
+     * @brief Making unit dual object.
+     *
+     * @tparam V type of constant value.
+     * @param v constant value.
+     * @param size the number of variables.
+     * @param position 
+     *
+     * @return 
+     */
     template <typename V>
-    dual<V> make_variable(
+    dual<V> 
+    make_variable(
         const V& v, 
         const std::size_t size, 
         const std::size_t position)
@@ -21,7 +51,35 @@ namespace ddd { namespace ad {
         return dual<V>(v, d);
     }
 
+    /**
+     * @brief 
+     *
+     * @tparam V
+     * @param v
+     * @param size
+     *
+     * @return 
+     */
+    template <typename V>
+    ublas::vector<dual<V> > 
+    make_unit_vector_dual(
+        const V& v,
+        const std::size_t size)
+    {
+        typedef ublas::vector<V> infinitesimal_type;
+        typedef dual<V, infinitesimal_type> dual_type;
+        typedef ublas::vector<dual_type> result_type;
+
+        result_type unitVectorDual(size);
+
+        for (std::size_t i = 0; i < size; ++i) {
+            unitVectorDual(i) = make_variable(v, size, i);
+        }
+
+        return unitVectorDual;
+    }
     
 } } // namespace ddd { namespace ad {
 
 #endif // #ifndef DDD_AD_HELPER_FUNCTION_H_INCLUDED
+
--- yield_curve_toy02/ad/jacobian_matrix.h
+++ yield_curve_toy02/ad/jacobian_matrix.h
@@ -1,111 +1,389 @@
+/**
+ * @file jacobian_matrix.h
+ * @brief 
+ * @author i05nagai
+ * @version 0.0.1
+ * @date 2016-03-27
+ */
+
 #ifndef DDD_AD_JACOBIAN_MATRIX_H_INCLUDED
 #define DDD_AD_JACOBIAN_MATRIX_H_INCLUDED
 
-#include "fwd.h"
 #include <boost/numeric/ublas/matrix_expression.hpp>
+#include "fwd.h"
 
 namespace ddd { namespace ad {
-    //(a^{1}+b^{1}d, ..., a^{M}+b^{M}d) 
-    // or 
-    //(a^{1}+b^{1}_{1}d_{1}+...+b^{1}_{N}d_{N}, 
-    //..., 
-    //a^{M}+b^{M}_{1}d_{1}+...+b^{M}_{N}d_{N}) 
-//    template <typename D>
-//    struct dual_jacobian_matrix_traits<D, false> {
-//    public:
-//        typedef typename detail::element_traits<D>::type 
-//            value_type;
-//        typedef typename value_type::infinitesmal_type 
-//            infinitesmal_type;
-//        typedef ublas::matrix<value_type> result_type;
-//
-//    public:
-//        //D is vector
-//        result_type apply(const D& x)
-//        {
-//            result_type jac(x.size(), D::size_value);
-//
-//            for (std::size_t i = 0; i < x.size(); ++i) {
-//                for (std::size_t j = 0; j < D::size_value; ++j) {
-//                    jac(i, j) = x(i).d()(j);
-//                }
-//            }
-//            return jac;
-//        }
-//    };
-//
 
-    //TODO:
+    namespace detail {
+
+        /**
+         * @brief
+         * D is vector<dual<V, I> >.
+         * V is scalar.
+         * I is vector.
+         */
+        template <typename D>
+        class jacobian_matrix_adapter_vector_dual<D, true> {
+        private:
+            //self_type
+            typedef jacobian_matrix_adapter_vector_dual<D, true> self_type;
+            //dual_type
+            typedef D dual_type;
+            typedef typename D::value_type element_type;
+            typedef typename element_type::infinitesimal_type 
+                infinitesimal_type;
+            typedef typename infinitesimal_type::value_type 
+                infinitesimal_value_type;
+        public:
+            //expression_type
+            typedef typename D::const_closure_type expression_closure_type;
+            //size_type
+            typedef std::size_t size_type;
+            //difference
+            typedef std::ptrdiff_t difference_type;
+            //reference
+            typedef infinitesimal_value_type const_reference;
+            typedef const_reference reference;
+            //closure
+            typedef const self_type const_closure_type;
+            typedef self_type closure_type;
+        public:
+            jacobian_matrix_adapter_vector_dual(const dual_type& e)
+            : _e(e), _size1(_e.size()), _size2(_e(0).d().size())
+            {
+            }
+
+            //TODO:need copy constructor?
+
+            const_reference operator()(
+                size_type i, 
+                size_type j) const
+            {
+                assert(i >= 0);
+                assert(_size1 >= i);
+                assert(j >= 0);
+                assert(_size2 >= j);
+                return _e(i).d()(j);
+            }
+
+            reference operator()(
+                size_type i, 
+                size_type j)
+            {
+                return _e(i).d()(j);
+            }
+
+            size_type size1() const 
+            {
+                return _size1;
+            }
+
+            size_type size2() const 
+            {
+                return _size2;
+            }
+        
+        private:
+            expression_closure_type _e;
+            size_type _size1;
+            size_type _size2;
+        };
+
+        /**
+         * @brief
+         * D is vector<dual<V, I> >.
+         * V is scalar.
+         * I is scalar.
+         */
+        template <typename D>
+        class jacobian_matrix_adapter_vector_dual<D, false> {
+        private:
+            //self_type
+            typedef jacobian_matrix_adapter_vector_dual<D, false> self_type;
+            typedef D dual_type;
+            typedef typename D::value_type element_type;
+            typedef typename element_type::infinitesimal_type 
+                infinitesimal_type;
+        public:
+            //expression_type
+            typedef typename D::const_closure_type expression_closure_type;
+            //size
+            typedef std::size_t size_type;
+            //difference
+            typedef std::ptrdiff_t difference_type;
+            //reference
+            typedef infinitesimal_type const_reference;
+            typedef const_reference reference;
+            //closure
+            typedef const self_type const_closure_type;
+            typedef self_type closure_type;
+        public:
+            jacobian_matrix_adapter_vector_dual(const dual_type& e)
+            : _e(e), _size1(_e.size()), _size2(1)
+            {
+            }
+            
+            //TODO:need copy constructor?
+
+            const_reference operator()(
+                size_type i, 
+                size_type j) const
+            {
+                assert(i >= 0);
+                assert(_size1 > i);
+                assert(j == 0);
+                return _e(i).d();
+            }
+
+            reference operator()(
+                size_type i, 
+                size_type j)
+            {
+                assert(i >= 0);
+                assert(_size1 > i);
+                assert(j == 0);
+                return _e(i).d();
+            }
+
+            size_type size1() const 
+            {
+                return _size1;
+            }
+
+            size_type size2() const 
+            {
+                return _size2;
+            }
+
+        private:
+            expression_closure_type _e;
+            size_type _size1;
+            size_type _size2;
+        };
+
+        /**
+         * @brief
+         * D is dual<V, I>.
+         * V is scalar.
+         * I is vector.
+         */
+        template <typename D>
+        class jacobian_matrix_adapter_scalar_dual<D, true> {
+        private:
+            //self_type
+            typedef jacobian_matrix_adapter_scalar_dual<D, true> self_type;
+            //dual_type
+            typedef D dual_type;
+            //infinitesimal_type
+            typedef typename D::infinitesimal_type 
+                infinitesimal_type;
+            typedef typename infinitesimal_type::value_type 
+                infinitesimal_value_type;
+        public:
+            //expression_type
+            typedef typename D::const_closure_type expression_closure_type;
+            //size_type
+            typedef std::size_t size_type;
+            //difference
+            typedef std::ptrdiff_t difference_type;
+            //reference
+            typedef infinitesimal_value_type const_reference;
+            typedef const_reference reference;
+            //closure
+            typedef const self_type const_closure_type;
+            typedef self_type closure_type;
+        public:
+            jacobian_matrix_adapter_scalar_dual(const dual_type& e)
+            : _e(e), _size1(1), _size2(_e.d().size())
+            {
+            }
+
+            //TODO:need copy constructor?
+
+            const_reference operator()(
+                size_type i, 
+                size_type j) const
+            {
+                assert(i == 0);
+                assert(j >= 0);
+                assert(j < _size2);
+                return _e.d()(j);
+            }
+
+            reference operator()(
+                size_type i, 
+                size_type j)
+            {
+                assert(i == 0);
+                assert(j >= 0);
+                assert(j < _size2);
+                return _e.d()(j);
+            }
+
+            size_type size1() const 
+            {
+                return _size1;
+            }
+
+            size_type size2() const 
+            {
+                return _size2;
+            }
+        
+        private:
+            expression_closure_type _e;
+            size_type _size1;
+            size_type _size2;
+        };
+
+        /**
+         * @brief
+         * D is dual<V, I>.
+         * V is scalar.
+         * I is scalar.
+         */
+        template <typename D>
+        class jacobian_matrix_adapter_scalar_dual<D, false> {
+        private:
+            //self_type
+            typedef jacobian_matrix_adapter_scalar_dual<D, false> self_type;
+            //dual_type
+            typedef D dual_type;
+            //infinitesimal_type
+            typedef typename D::infinitesimal_type infinitesimal_type;
+        public:
+            //expression_type
+            typedef typename D::const_closure_type expression_closure_type;
+            //size_type
+            typedef std::size_t size_type;
+            //difference
+            typedef std::ptrdiff_t difference_type;
+            //reference
+            typedef infinitesimal_type const_reference;
+            typedef const_reference reference;
+            //closure
+            typedef const self_type const_closure_type;
+            typedef self_type closure_type;
+        public:
+            jacobian_matrix_adapter_scalar_dual(const dual_type& e)
+            : _e(e)
+            {
+            }
+
+            //TODO:need copy constructor?
+
+            const_reference operator()(
+                size_type i, 
+                size_type j) const
+            {
+                assert(i == 0);
+                assert(j == 0);
+                return _e.d();
+            }
+
+            reference operator()(
+                size_type i, 
+                size_type j)
+            {
+                assert(i == 0);
+                assert(j == 0);
+                return _e.d();
+            }
+
+            size_type size1() const 
+            {
+                return 1;
+            }
+
+            size_type size2() const 
+            {
+                return 1;
+            }
+
+        private:
+            expression_closure_type _e;
+        };
+
+        template <typename D>
+        class jacobian_matrix_adapter_impl<D, true>
+        : public jacobian_matrix_adapter_vector_dual<D> {
+        private:
+            typedef D dual_type;
+        public:
+            jacobian_matrix_adapter_impl(const dual_type& e)
+            : jacobian_matrix_adapter_vector_dual<D>(e)
+            {
+            }
+        };
+
+        template <typename D>
+        class jacobian_matrix_adapter_impl<D, false>
+        : public jacobian_matrix_adapter_scalar_dual<D> {
+        private:
+            typedef D dual_type;
+        public:
+            jacobian_matrix_adapter_impl(const dual_type& e)
+            : jacobian_matrix_adapter_scalar_dual<D>(e)
+            {
+            }
+        };
+
+    } // namespace detail
+
+    /*--------------------------------------------------------------------------
+     * dual_jacobian_matrix_adapter
+     *------------------------------------------------------------------------*/
     template <typename D>
     class dual_jacobian_matrix_adapter 
     : ublas::matrix_expression<dual_jacobian_matrix_adapter<D> > {
     private:
-        typedef dual_jacobian_matrix_adapter<D> self_type;
-        //D::value_type must be dual.
-        typedef typename D::inf_type inf_type;//inf_type can be integral.
-    public:
         typedef D dual_type;
-        typedef typename D::const_closure_type expression_closure_type;
-        typedef typename inf_type::value_type value_type; 
-        typedef const value_type& const_reference;
-        typedef value_type& reference;
+        //self_type
+        typedef dual_jacobian_matrix_adapter<D> self_type;
+        //impl
+        typedef typename detail::jacobian_matrix_adapter_impl<D> _Impl;
+    public:
+        //size_type
+        typedef typename _Impl::size_type size_type;
+        //reference
+        typedef typename _Impl::const_reference const_reference;
+        typedef typename _Impl::reference reference;
+        //closure
         typedef const ublas::matrix_reference<const self_type> 
             const_closure_type;
         typedef ublas::matrix_reference<self_type> closure_type;
 
         // Construction and destruction
         dual_jacobian_matrix_adapter(const dual_type& e)
-        : _size1(e.size()), _size2(e.d().size()), _e(e)
+        : _impl(e)
         {
+        }
+
+        const_reference operator()(size_type i, size_type j) const 
+        {
+            return _impl(i, j);
+        }
+
+        reference operator()(size_type i, size_type j) 
+        {
+            return _impl(i, j);
         }
 
         // Accessors
         size_type size1() const 
         {
-            return _size1;
+            return _impl.size1();
         }
 
         size_type size2() const 
         {
-            return _size2;
+            return _impl.size2();
         }
 
+        //TODO:define interator
+
     private:
-        expression_closure_type _e;
-        size_type _size1;
-        size_type _size2;
-    };
-
-
-
-    //a+bd or a+b_{1}d_{1}+...+b_{N}d_{N}
-//    template <typename D>
-//    struct dual_jacobian_matrix_traits<D, true> {
-//    public:
-//        typedef typename D::value_type value_type;
-//        typedef typename D::infinitesmal_type infinitesmal_type;
-//        typedef ublas::vector<value_type> result_type;
-//    public:
-//        //D is scalar
-//        result_type apply(const D& x)
-//        {
-//            result_type jac(D::size_value);
-//
-//            for (std::size_t i = 0; i < D::size_value; ++i) {
-//                jac(i) = x.d()(i);
-//            }
-//            
-//            return jac;
-//        }
-//    };
-
-
-    template <typename D>
-    struct jacobian_matrix_traits<D, false>;
-
-    template <typename D>
-    struct jacobian_matrix_traits<D, true> 
-    : public dual_jacobian_matrix_traits<D> {
+        _Impl _impl;
     };
 
 } } // namespace ddd { namespace ad {
